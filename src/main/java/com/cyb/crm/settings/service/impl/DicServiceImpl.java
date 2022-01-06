0f1c197426b4f7332453f460afbb803aec52afe9
ccc <895739673@qq.com>
2022-01-06 11:19:23 +0800

    23

--- src/main/java/com/cyb/crm/settings/service/impl/DicServiceImpl.java
+++ src/main/java/com/cyb/crm/settings/service/impl/DicServiceImpl.java
@@ -8,6 +8,7 @@ import com.cyb.crm.settings.domain.DicValue;
 import com.cyb.crm.settings.service.DicService;
 import com.cyb.crm.utils.SqlSessionUtil;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +18,7 @@ public class DicServiceImpl implements DicService {
 
     @Override
     public Map<String, List<DicValue>> getAll() {
+        Map<String, List<DicValue>> map = new HashMap<String, List<DicValue>>();
         //将字典类型列表取出，7种
         List<DicType> dtList =  dicTypeDao.getTypeList();
         //价格字典类型列表遍历
@@ -24,8 +26,10 @@ public class DicServiceImpl implements DicService {
             //取得每一种类型的字典类型编码appellation、clueState...
             String code = dt.getCode();
             //根据每一个字典类型，来取得字典纸列表
-            List<DicValue> dvList = dicValueDao.getListByCode(code);
+            List<DicValue> dvList = dicValueDao.getListByCode(code);//通过获取的类型code，将每种类型的值value保存到dvList
+            map.put(code+"List",dvList);
         }
+        return map;
     }
 }
 
